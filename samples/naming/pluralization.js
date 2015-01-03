11876c16dfb401e30d6c268f8af767fb75b73096
Wesley McClure <wes.mcclure@gmail.com>
2015-01-03 17:38:09 -0500

    Add pluralization examples

---
+++ samples/naming/pluralization.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var assert = require('better-assert');
+
+describe('pluralization', function () {
+  it('how about entry and entryItem?', function () {
+
+    var entry = [
+      {
+        description: 'Meal',
+        amount: 500
+      },
+      {
+        description: 'Taxi',
+        amount: 200
+      }
+    ];
+
+    var total = 0;
+    var entryItem;
+    for (var index = 0; index < entry.length; index++) {
+      entryItem = entry[index];
+      total += entryItem.amount;
+    }
+
+    assert(total === 700);
+  });
+
+  it('how about entryList and entry?', function () {
+
+    var entryList = [
+      {
+        description: 'Meal',
+        amount: 500
+      },
+      {
+        description: 'Taxi',
+        amount: 200
+      }
+    ];
+
+    var total = 0;
+    var entry;
+    for (var index = 0; index < entryList.length; index++) {
+      entry = entryList[index];
+      total += entry.amount;
+    }
+
+    assert(total === 700);
+  });
+
+  it('how about entries and entry?', function () {
+
+    var entries = [
+      {
+        description: 'Meal',
+        amount: 500
+      },
+      {
+        description: 'Taxi',
+        amount: 200
+      }
+    ];
+
+    var total = 0;
+    var entry;
+    for (var index = 0; index < entries.length; index++) {
+      entry = entries[index];
+      total += entry.amount;
+    }
+
+    assert(total === 700);
+  });
+
+});
+
