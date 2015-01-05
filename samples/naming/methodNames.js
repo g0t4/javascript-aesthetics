a97b546ddfbf655343e51b4e048c1b0c3449a145
Wesley McClure <wes.mcclure@gmail.com>
2015-01-05 00:37:39 -0500

    Method name examples for naming

---
+++ samples/naming/methodNames.js
@@ -0,0 +1,38 @@
+describe("methodNames", function () {
+    "use strict";
+    /*eslint no-unused-vars: 0, no-undef: 0 */
+
+    it("how about this?", function () {
+
+        var p = m(args);
+
+        //var p = max(args);
+
+        var maximumCost = maximum(prices);
+
+        account.credit(10);
+        account.withdrawl(5);
+
+        account.getBalance();
+        account.getPendingTransactions();
+
+        account.balance();
+        account.pendingTransactions();
+
+        database.save(person);
+        database.saveAndReturnId(person);
+
+        function doLaundryAndWashCarAndFeedCat() { }
+
+        function doLaundry() {  }
+
+        function washCar() {  }
+
+        function feedCat() {  }
+
+        function getWeatherHistoryAndPredictWeeklyForecast(city) {  }
+
+        function getWeeklyForecast(city) {  }
+
+    });
+});
