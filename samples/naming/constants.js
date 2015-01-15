a9e2e6c5b67b6fc5b702538f3c31bb6fc53119d4
Wesley McClure <wes.mcclure@gmail.com>
2015-01-15 17:10:14 -0500

    Get tests running for samples

--- samples/naming/constants.js
+++ samples/naming/constants.js
@@ -1,10 +1,12 @@
 /*eslint strict: 0, no-undef: 0, no-unused-vars: 0, no-redeclare: 0*/
 
-var c = 2 * 3.14159 * r;
+function constants() {
+    var c = 2 * 3.14159 * r;
 
-var PI = 3.14159;
-var c = 2 * PI * r;
+    var PI = 3.14159;
+    var c = 2 * PI * r;
 
-var circumference = 2 * PI * radius;
+    var circumference = 2 * PI * radius;
 
-var DAYS_IN_WEEK = 7;
+    var DAYS_IN_WEEK = 7;
+}
--- samples/naming/translation.js
+++ samples/naming/translation.js
@@ -1,13 +1,17 @@
 /*eslint strict: 0, no-undef: 0, no-unused-vars: 0, no-redeclare: 0*/
 
-// a bucket comes to mind as an abstraction for tracking storage
-var bucket = new Bucket(40);
-bucket.store(20);
-bucket.remove(10);
-console.log(bucket.getFreeSpace());
+function translation() {
 
-// but you're really working with warehouses
-var warehouse = new Warehouse(40);
-warehouse.store(20);
-warehouse.remove(10);
-console.log(warehouse.getFreeSpace());
+    // a bucket comes to mind as an abstraction for tracking storage
+    var bucket = new Bucket(40);
+    bucket.store(20);
+    bucket.remove(10);
+    console.log(bucket.getFreeSpace());
+
+    // but you're really working with warehouses
+    var warehouse = new Warehouse(40);
+    warehouse.store(20);
+    warehouse.remove(10);
+    console.log(warehouse.getFreeSpace());
+
+}
