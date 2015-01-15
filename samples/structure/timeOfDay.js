1a276743003fc2da855d28f847d504fa59391011
Wesley McClure <wes.mcclure@gmail.com>
2015-01-15 17:17:37 -0500

    Garble the format of the timeOfDay method for demonstration purposes

--- samples/structure/timeOfDay.js
+++ samples/structure/timeOfDay.js
@@ -2,28 +2,28 @@ var assert = require("better-assert");
 
 describe("structure", function () {
     "use strict";
+    /*eslint curly: 0, space-infix-ops: 0, no-multi-spaces: 0, semi: 0, no-spaced-func: 0 */
 
-    it("should compute the proper day of week", function () {
+    it("should compute the proper time of day", function () {
 
-        function timeOfDay(date) {
-            if (date.getHours() < 6) {
-                return "Too Early!";
-            }
-            else if (date.getHours() < 12) {
-                return "Morning";
-            }
-            else if (date.getHours() < 16) {
+        function timeOfDay(
+            date){
+            if (date.getHours()<12&&date.getHours()>5){
+                return "Morning"}
+               else
+                if(date .getHours ()<6)
+                        return "Too Early!";
+            else    if    (       date. getHours()    <      16)  {
                 return "Afternoon";
-            }
-            else if (date.getHours() < 20) {
-                return "Evening";
-            }
-            else {
-                return "Night";
-            }
+        }
+              else if ((date.getHours () <   20) ) return "Evening";
+                else return "Night"
         }
 
         assert(timeOfDay(new Date(2015, 1, 15, 18, 0)) === "Evening");
+        assert(timeOfDay(new Date(2015, 1, 15, 21, 0)) === "Night");
+        assert(timeOfDay(new Date(2015, 1, 15, 5, 0)) === "Too Early!");
+        assert(timeOfDay(new Date(2015, 1, 15, 10, 0)) === "Morning");
 
     });
 
